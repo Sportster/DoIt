1eb3113c36a0b20eff7983198aed078de98097dc
Diego <mrdgomez4@gmail.com>
2017-01-14 18:45:05 -0500

    Added CreateTaskViewController
    Added CreateTaskViewController

---
+++ CreateTaskViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Diego Gomez on 2017-01-14.
+//  Copyright © 2017 Diego Gomez. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBOutlet weak var taskNameField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        
+        
+    }
+  
+}
