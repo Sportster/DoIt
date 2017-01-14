ff0f69ccbeefaba886786ffe46ee0d2e40bbb3f4
Diego <mrdgomez4@gmail.com>
2017-01-13 23:46:26 -0500

    Initial commit
    Initial commit

---
+++ Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Diego Gomez on 2017-01-13.
+//  Copyright © 2017 Diego Gomez. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+}
