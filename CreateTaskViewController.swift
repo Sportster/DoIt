564bca1d1399a07be281f4a57d94af7d5dddfa4a
Diego <mrdgomez4@gmail.com>
2017-01-16 10:21:54 -0500

    Added TasksViewController
    Created a new viewController

--- CreateTaskViewController.swift
+++ CreateTaskViewController.swift
@@ -9,18 +9,31 @@
 import UIKit
 
 class CreateTaskViewController: UIViewController {
-
+    
     @IBOutlet weak var importantSwitch: UISwitch!
     @IBOutlet weak var taskNameField: UITextField!
+    
+    var previousVC = TasksViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func addTapped(_ sender: Any) {
+        // create taask from the outlet information
+        
+        let task = Task()
+        task.name = taskNameField.text!
+        task.important = importantSwitch.isOn
         
         
+        //Add new task to array in previous viewcontroller
+        
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController!.popViewController(animated: true)
     }
-  
+    
 }
